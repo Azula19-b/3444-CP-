eb5c889481740e0c9dc669951c10f3e9cc12c57e
Kingston Armstrong <70528667+Kiingst@users.noreply.github.com>
2026-02-24 12:26:32 -0600

    update

--- server.js
+++ server.js
@@ -6,17 +6,33 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// How the database works:
+// We have two tables Resturants and foods
+// they are connected by the restaurant_id which connnects the two
+// every food needs to needs to have a restaurant 
 const db = new sqlite3.Database("./database.db")
 db.serialize(() => {
+
+    db.run(`
+    CREATE TABLE IF NOT EXISTS restaurants (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      location TEXT,
+      UNIQUE(name, location)
+    )
+  `)
+
     db.run(`
         CREATE TABLE IF NOT EXISTS foods (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
+            restaurant_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
             category TEXT,
             price REAL,
             rating REAL,
             image TEXT, 
-            Location TEXT
+            UNIQUE(name, restaurant_id),
+            FOREIGN KEY (restaurant_id) REFERENCES restaurants(id) ON DELETE CASCADE
         )
     `)
 })
@@ -31,10 +47,24 @@ app.get("/", (req, res) => {
 })
 
 app.post("/add-food", (req, res) => {
-    const { name, category, price, rating, Location, image } = req.body
+    const { name, category, price, rating, image } = req.body
     db.run(
-        `INSERT INTO foods (name, category, price, rating, Location, image) VALUES (?, ?, ?, ?, ?, ?)`,
-        [name, category, price, rating, Location, image],
+        `INSERT INTO foods (name, restaurant_id, category, price, rating, image) VALUES (?, ?, ?, ?, ?, ?)`,
+        [name, req.body.restaurant_id, category, price, rating, image],
+        function (err) {
+            if (err) {
+                return res.status(500).json({ error: err.message })
+            }
+            res.json({ id: this.lastID })
+        }
+    )
+})
+
+app.post("/add-restaurant", (req, res) => {
+    const { name, location } = req.body
+    db.run(
+        `INSERT INTO restaurants (name, location) VALUES (?, ?)`,
+        [name, location],
         function (err) {
             if (err) {
                 return res.status(500).json({ error: err.message })
@@ -46,6 +76,15 @@ app.post("/add-food", (req, res) => {
 
 app.get("/foods", (req, res) => {
     db.all(`SELECT * FROM foods`, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message })
+        }
+        res.json(rows)
+    })
+})
+
+app.get("/restaurants", (req, res) => {
+    db.all(`SELECT * FROM restaurants`, [], (err, rows) => {
         if (err) {
             return res.status(500).json({ error: err.message })
         }
